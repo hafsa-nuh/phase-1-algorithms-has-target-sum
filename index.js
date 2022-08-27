349a5c9202127f5c9f41bfdebe88254ca413abe5
hafsa-nuh <109968509+hafsa-nuh@users.noreply.github.com>
2022-08-27 22:23:09 +0300

    done

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i=0; i < array.length; i++){
+    const subtractTargetArrau = target - array[i]
+    for (let j=i+1; j< array.length; j++){
+      if (array[j] === subtractTargetArrau)
+      return true;
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+
 */
+
 
 /* 
   Add your pseudocode here
+  ([22, 19, 4, 6, 30], 25)) 25-22 =3 is 3 inthe array 
+  if any number is adding up to the target return true else false
 */
 
 /*
   Add written explanation of your solution here
+ hasTargetSum([3, 8, 12, 4, 11, 7], 10)) looking for 10 in the array
+
 */
 
 // You can run `node index.js` to view these console logs
